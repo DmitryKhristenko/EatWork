30594fb21650ebaa52d682c7c390f3bf9eb64f05
Dmitry Khristenko <dmitrykhrist@gmail.com>
2023-09-05 14:17:35 +0300

    Initial Commit

---
+++ EatWork/EatWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  EatWorkApp.swift
+//  EatWork
+//
+//  Created by Дмитрий Х on 05.09.23.
+//
+
+import SwiftUI
+
+@main
+struct EatWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
